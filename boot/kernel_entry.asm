d470472d293be35ec88a5de4ee13ba48e7ed1ae8
Lachlan Jowett <lochyjowett1@gmail.com>
2022-11-02 22:20:36 +1100

    removed some old code

--- boot/kernel_entry.asm
+++
@@ -1,9 +0,0 @@
-; Unused code
-
-global _start
-[bits 32]
-
-_start:
-    [extern kernel_main] ; call the main function from kernel.c
-    call kernel_main ; Calls the kernel main function
-    jmp $
