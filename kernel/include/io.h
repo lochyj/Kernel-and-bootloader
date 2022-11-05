381a38e5992564b46315fbfbf178534521114c4a
Lachlan Jowett <lochyjowett1@gmail.com>
2022-11-05 22:02:55 +1100

    working on fixing STUPID compilation errors :(

--- kernel/include/io.h
+++ kernel/include/io.h
@@ -2,6 +2,18 @@
 
 // #include "../drivers/keyboard.h"
 
+
+
+unsigned char inb(unsigned short port) {
+    unsigned char result;
+    __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
+    return result;
+}
+
+void outb(unsigned short port, unsigned char data) {
+    __asm__("out %%al, %%dx" : : "a" (data), "d" (port));
+}
+
 int strlen(char* str) {
   int len = 0;
   while(str[len]) len++;
